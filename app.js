0485a0c2faeade7a2674bbc9dbfee559396cfcd8
Hanna Walgrave <Walgrave.hanna@hotmail.com>
2017-05-06 12:28:28 +0200

    added sass

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var sassMiddleware = require('node-sass-middleware');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
