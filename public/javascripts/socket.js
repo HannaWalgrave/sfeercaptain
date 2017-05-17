aab98050dffea28ee5150e36be8a3f42db682aa5
Hanna Walgrave <Walgrave.hanna@hotmail.com>
2017-05-17 16:23:27 +0200

    added websockets

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,4 +1,10 @@
-//var socket_io = require('socket.io');
-var io = socket_io();
+
 var socket = io();
 
+//socket.emit('clientEvent', 'Sent an event from the client!');
+//socket.on('message', function(data){document.write(data)});
+
+/*socket.on('broadcast',function(data){
+    document.body.innerHTML = '';
+    document.write(data.description);
+});*/
